75d657703f127c68a10001b156c32050e125779b
Thabiso <thabisoma@bbd.co.za>
2021-05-17 03:23:30 +0200

    made top nav fixed

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -4789,7 +4789,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62473" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60983" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
